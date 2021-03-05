7b568b23364609deec56d2a3147eb98c501fda1b
Alexandr <Aronin.2001@gmail.com>
2021-03-06 00:49:22 +0400

    add files

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+// 'use strict'
+
+// let nav = document.querySelector('nav'),
+//     ul = document.querySelectorAll('ul'),
+//     link = document.querySelectorAll('a')[1];
+
+// nav.addEventListener('mouseover', function(event) {
+//     if(event.target == link) {
+//         ul[1].style.display = "block";
+//     }
+// });
